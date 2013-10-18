31a9f575e6eda395aa0504c79078ab8e090393eb
wouldgo <wouldgo84@gmail.com>
2013-10-18 10:59:18 +0200

    added some ECB tests

--- spec/2-fish-ECB.js
+++ spec/2-fish-ECB.js
@@ -19,122 +19,202 @@ describe('Twofish ECB', function() {
   });
 
   it('6 char length key - single simple character', function() {
-    var pt = 'A'
-      , ct = twoFish.encrypt('secret', pt)
-      , cpt = twoFish.decrypt('secret', ct)
+    var key = 'secret'
+      , pt = 'A'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
       , ok = utils.areEqual(pt, cpt);
 
-    //console.info('Plaint Text: ', pt, '\r\nChiper Text: ', ct, '\r\nChipered Plaint Text: ', cpt);
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
     expect(ok).toEqual(true);
   });
 
-  it('6 char length key - multiple simple character', function() {
-    var pt = 'Super secret message!'
-      , ct = twoFish.encrypt('secret', pt)
-      , cpt = twoFish.decrypt('secret', ct)
+  it('6 char length key - multiple simple characters', function() {
+    var key = 'secret'
+      , pt = 'Super secret message!'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
       , ok = utils.areEqual(pt, cpt);
 
-    //console.info('Plaint Text: ', pt, '\r\nChiper Text: ', ct, '\r\nChipered Plaint Text: ', cpt);
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
     expect(ok).toEqual(true);
   });
 
   it('6 char length key - single "esotic" character', function() {
-    var pt = '☭'
-      , ct = twoFish.encrypt('secret', pt)
-      , cpt = twoFish.decrypt('secret', ct)
+    var key = 'secret'
+      , pt = '☭'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
       , ok = utils.areEqual(pt, cpt);
 
-    //console.info('Plaint Text: ', pt, '\r\nChiper Text: ', ct, '\r\nChipered Plaint Text: ', cpt);
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
     expect(ok).toEqual(true);
   });
 
-  it('6 char length key - multiple "esotic" character', function() {
-    var pt = '☢€  ☂ ڴ!'
-      , ct = twoFish.encrypt('secret', pt)
-      , cpt = twoFish.decrypt('secret', ct)
+  it('6 char length key - multiple "esotic" characters', function() {
+    var key = 'secret'
+      , pt = '☢€  ☂ ڴ!'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
       , ok = utils.areEqual(pt, cpt);
 
-    //console.info('Plaint Text: ', pt, '\r\nChiper Text: ', ct, '\r\nChipered Plaint Text: ', cpt);
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
     expect(ok).toEqual(true);
   });
 
   it('8 char length key - single simple character', function() {
-    var pt = 'A'
-      , ct = twoFish.encrypt('secret12', pt)
-      , cpt = twoFish.decrypt('secret12', ct)
+    var key = 'secret12'
+      , pt = 'A'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
       , ok = utils.areEqual(pt, cpt);
 
-    //console.info('Plaint Text: ', pt, '\r\nChiper Text: ', ct, '\r\nChipered Plaint Text: ', cpt);
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
     expect(ok).toEqual(true);
   });
 
-  it('8 char length key - multiple simple character', function() {
-    var pt = 'Sunday monday, happy days!'
-      , ct = twoFish.encrypt('secret12', pt)
-      , cpt = twoFish.decrypt('secret12', ct)
+  it('8 char length key - multiple simple characters', function() {
+    var key = 'secret12'
+      , pt = 'Sunday monday, happy days!'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
       , ok = utils.areEqual(pt, cpt);
 
-    //console.info('Plaint Text: ', pt, '\r\nChiper Text: ', ct, '\r\nChipered Plaint Text: ', cpt);
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
     expect(ok).toEqual(true);
   });
 
   it('8 char length key - single "esotic" character', function() {
-    var pt = '☂'
-      , ct = twoFish.encrypt('secret12', pt)
-      , cpt = twoFish.decrypt('secret12', ct)
+    var key = 'secret12'
+      , pt = '☂'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
       , ok = utils.areEqual(pt, cpt);
 
-    //console.info('Plaint Text: ', pt.charCodeAt(0), '\r\nChiper Text: ', ct, '\r\nChipered Plaint Text: ', cpt);
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
     expect(ok).toEqual(true);
   });
 
-  it('8 char length key - multiple "esotic" character', function() {
-    var pt = '☭☢€  ☂ ڴ'
-      , ct = twoFish.encrypt('secret12', pt)
-      , cpt = twoFish.decrypt('secret12', ct)
+  it('8 char length key - multiple "esotic" characters', function() {
+    var key = 'secret12'
+      , pt = '☭☢€  ☂ ڴ'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
       , ok = utils.areEqual(pt, cpt);
 
-    //console.info('Plaint Text: ', pt.charCodeAt(0), '\r\nChiper Text: ', ct, '\r\nChipered Plaint Text: ', cpt);
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
+    expect(ok).toEqual(true);
+  });
+
+  it('13 char length key - single simple character', function() {
+    var key = 'anothersecret'
+      , pt = 'A'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
+      , ok = utils.areEqual(pt, cpt);
+
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
+    expect(ok).toEqual(true);
+  });
+
+  it('13 char length key - multiple simple characters', function() {
+    var key = 'anothersecret'
+      , pt = 'Super secret message!'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
+      , ok = utils.areEqual(pt, cpt);
+
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
+    expect(ok).toEqual(true);
+  });
+
+  it('13 char length key - single "esotic" character', function() {
+    var key = 'anothersecret'
+      , pt = '☭'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
+      , ok = utils.areEqual(pt, cpt);
+
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
+    expect(ok).toEqual(true);
+  });
+
+  it('13 char length key - multiple "esotic" characters', function() {
+    var key = 'anothersecret'
+      , pt = '☢€  ☂ ڴ!'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
+      , ok = utils.areEqual(pt, cpt);
+
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
     expect(ok).toEqual(true);
   });
 
   it('37 char length key - single simple character', function() {
-    var pt = 'A'
-      , ct = twoFish.encrypt('A Super secret that can not be stolen', pt)
-      , cpt = twoFish.decrypt('A Super secret that can not be stolen', ct)
+    var key = 'A Super secret that can not be stolen'
+      , pt = 'A'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
       , ok = utils.areEqual(pt, cpt);
 
-    //console.info('Plaint Text: ', pt, '\r\nChiper Text: ', ct, '\r\nChipered Plaint Text: ', cpt);
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
     expect(ok).toEqual(true);
   });
 
-  it('37 char length key - multiple simple character', function() {
-    var pt = 'Sunday monday, happy days!'
-      , ct = twoFish.encrypt('A Super secret that can not be stolen', pt)
-      , cpt = twoFish.decrypt('A Super secret that can not be stolen', ct)
+  it('37 char length key - multiple simple characters', function() {
+    var key = 'A Super secret that can not be stolen'
+      , pt = 'Sunday monday, happy days!'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
       , ok = utils.areEqual(pt, cpt);
 
-    //console.info('Plaint Text: ', pt, '\r\nChiper Text: ', ct, '\r\nChipered Plaint Text: ', cpt);
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
     expect(ok).toEqual(true);
   });
 
   it('37 char length key - single "esotic" character', function() {
-    var pt = '☂'
-      , ct = twoFish.encrypt('A Super secret that can not be stolen', pt)
-      , cpt = twoFish.decrypt('A Super secret that can not be stolen', ct)
+    var key = 'A Super secret that can not be stolen'
+      , pt = '☂'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
       , ok = utils.areEqual(pt, cpt);
 
-    //console.info('Plaint Text: ', pt.charCodeAt(0), '\r\nChiper Text: ', ct, '\r\nChipered Plaint Text: ', cpt);
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
     expect(ok).toEqual(true);
   });
 
-  it('37 char length key - multiple "esotic" character', function() {
-    var pt = '☭☢€  ☂ ڴ'
-      , ct = twoFish.encrypt('A Super secret that can not be stolen', pt)
-      , cpt = twoFish.decrypt('A Super secret that can not be stolen', ct)
+  it('37 char length key - multiple "esotic" characters', function() {
+    var key = 'A Super secret that can not be stolen'
+      , pt = '☭☢€  ☂ ڴ'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
       , ok = utils.areEqual(pt, cpt);
 
-    //console.info('Plaint Text: ', pt.charCodeAt(0), '\r\nChiper Text: ', ct, '\r\nChipered Plaint Text: ', cpt);
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-');
     expect(ok).toEqual(true);
+  });
+
+  it('24 char length key - 32 characters, first 16 equals to second 16', function() {
+    var key = 'supasecret11oneoneeleven'
+      , pt = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
+      , firstArr = ct.slice(0, 16)
+      , secondArr = ct.slice(16, 32);
+
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-\r\n (0..16) Array: ', firstArr, '-\r\n(16..32) Array: ', secondArr, '-');
+    expect(firstArr).toEqual(secondArr);
+  });
+
+  it('24 char length key - 32 characters, first 16 equals to second 16 (2)', function() {
+    var key = 'supasecret11oneoneeleven'
+      , pt = 'ABCDABCDABCDABCDABCDABCDABCDABCD'
+      , ct = twoFish.encrypt(key, pt)
+      , cpt = twoFish.decrypt(key, ct)
+      , firstArr = ct.slice(0, 16)
+      , secondArr = ct.slice(16, 32);
+
+    console.info('Key: ', key, '-\r\nPlaint Text: ', pt, '-\r\nChiper Text: ', ct, '-\r\nChipered Plaint Text: ', cpt, '-\r\n (0..16) Array: ', firstArr, '-\r\n(16..32) Array: ', secondArr, '-');
+    expect(firstArr).toEqual(secondArr);
   });
 });
