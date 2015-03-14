5d0960770ad278b9c49c566c49fab5dcaa944a81
wouldgo <wouldgo84@gmail.com>
2015-03-14 12:31:47 +0100

    cbc on string completed

--- spec/cbc-iv-gt-16.js
+++ spec/cbc-iv-gt-16.js
@@ -9,297 +9,73 @@
 
           var key = twF.stringToByteArray(aKey)
             , pt = twF.stringToByteArray(aPt)
-            , ct = twF.encrypt(key, pt)
-            , cpt = twF.decrypt(key, ct);
+            , ct = twF.encryptCBC(key, pt)
+            , cpt = twF.decryptCBC(key, ct);
 
           expect(pt).toEqual(cpt);
-        };
-
-    it('6 char length key - single simple character', testCase.bind(this, 'secret', 'A'));
-    it('6 char length key - multiple simple characters', testCase.bind(this, 'secret', 'Super secret message!'));
-    it('6 char length key - single "esotic" character', testCase.bind(this, 'secret', '☭'));
-    it('6 char length key - multiple "esotic" characters', testCase.bind(this, 'secret', '☢€  ☂ ڴ!'));
-    it('8 char length key - single simple character', testCase.bind(this, 'secret12', 'A'));
-    it('8 char length key - multiple simple characters', testCase.bind(this, 'secret12', 'Sunday monday, happy days!'));
-    it('8 char length key - single "esotic" character', testCase.bind(this, 'secret12', '☂'));
-    it('8 char length key - multiple "esotic" characters', testCase.bind(this, 'secret12', '☭☢€  ☂ ڴ'));
-
-    /*it('13 char length key - single simple character', function() {
-
-      var key = twF.stringToByteArray('anothersecret')
-        , pt = twF.stringToByteArray('A')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('13 char length key - multiple simple characters', function() {
-
-      var key = twF.stringToByteArray('anothersecret')
-        , pt = twF.stringToByteArray('Super secret message!')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('13 char length key - single "esotic" character', function() {
-
-      var key = twF.stringToByteArray('anothersecret')
-        , pt = twF.stringToByteArray('☭')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('13 char length key - multiple "esotic" characters', function() {
-
-      var key = twF.stringToByteArray('anothersecret')
-        , pt = twF.stringToByteArray('☢€  ☂ ڴ!')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('6 char length key - multiple simple characters', function() {
-
-      var key = twF.stringToByteArray('secret')
-        , pt = twF.stringToByteArray('Super secret message!')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('6 char length key - single "esotic" character', function() {
-
-      var key = twF.stringToByteArray('secret')
-        , pt = twF.stringToByteArray('☭')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('6 char length key - multiple "esotic" characters', function() {
-
-      var key = twF.stringToByteArray('secret')
-        , pt = twF.stringToByteArray('☢€  ☂ ڴ!')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('8 char length key - single simple character', function() {
-
-      var key = twF.stringToByteArray('secret12')
-        , pt = twF.stringToByteArray('A')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('8 char length key - multiple simple characters', function() {
-
-      var key = twF.stringToByteArray('secret12')
-        , pt = twF.stringToByteArray('Sunday monday, happy days!')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('8 char length key - single "esotic" character', function() {
-
-      var key = twF.stringToByteArray('secret12')
-        , pt = twF.stringToByteArray('☂')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('8 char length key - multiple "esotic" characters', function() {
-
-      var key = twF.stringToByteArray('secret12')
-        , pt = twF.stringToByteArray('☭☢€  ☂ ڴ')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('13 char length key - single simple character', function() {
-
-      var key = twF.stringToByteArray('anothersecret')
-        , pt = twF.stringToByteArray('A')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('13 char length key - multiple simple characters', function() {
-
-      var key = twF.stringToByteArray('anothersecret')
-        , pt = twF.stringToByteArray('Super secret message!')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('13 char length key - single "esotic" character', function() {
-
-      var key = twF.stringToByteArray('anothersecret')
-        , pt = twF.stringToByteArray('☭')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('13 char length key - multiple "esotic" characters', function() {
-
-      var key = twF.stringToByteArray('anothersecret')
-        , pt = twF.stringToByteArray('☢€  ☂ ڴ!')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('37 char length key - single simple character', function() {
-
-      var key = twF.stringToByteArray('A Super secret that can not be stolen')
-        , pt = twF.stringToByteArray('A')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('37 char length key - multiple simple characters', function() {
-
-      var key = twF.stringToByteArray('A Super secret that can not be stolen')
-        , pt = twF.stringToByteArray('Sunday monday, happy days!')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('37 char length key - single "esotic" character', function() {
-
-      var key = twF.stringToByteArray('A Super secret that can not be stolen')
-        , pt = twF.stringToByteArray('☂')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('37 char length key - multiple "esotic" characters', function() {
-
-      var key = twF.stringToByteArray('A Super secret that can not be stolen')
-        , pt = twF.stringToByteArray('☭☢€  ☂ ڴ')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('24 char length key - 32 characters, first 16 equals to second 16', function() {
-
-      var key = twF.stringToByteArray('supasecret11oneoneeleven')
-        , pt = twF.stringToByteArray('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct)
-        , firstArr = ct.slice(0, 16)
-        , secondArr = ct.slice(16, 32);
-
-      expect(firstArr).not.toEqual(secondArr);
-    });
-
-    it('24 char length key - 32 characters, first 16 equals to second 16 (2)', function() {
-
-      var key = twF.stringToByteArray('supasecret11oneoneeleven')
-        , pt = twF.stringToByteArray('ABCDABCDABCDABCDABCDABCDABCDABCD')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct)
-        , firstArr = ct.slice(0, 16)
-        , secondArr = ct.slice(16, 32);
-
-      expect(firstArr).not.toEqual(secondArr);
-    });
-
-    it('37 char length key - single simple character', function() {
-
-      var key = twF.stringToByteArray('A Super secret that can not be stolen')
-        , pt = twF.stringToByteArray('A')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('37 char length key - multiple simple characters', function() {
-
-      var key = twF.stringToByteArray('A Super secret that can not be stolen')
-        , pt = twF.stringToByteArray('Sunday monday, happy days!')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('37 char length key - single "esotic" character', function() {
-
-      var key = twF.stringToByteArray('A Super secret that can not be stolen')
-        , pt = twF.stringToByteArray('☂')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('37 char length key - multiple "esotic" characters', function() {
-
-      var key = twF.stringToByteArray('A Super secret that can not be stolen')
-        , pt = twF.stringToByteArray('☭☢€  ☂ ڴ')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct);
-
-      expect(pt).toEqual(cpt);
-    });
-
-    it('24 char length key - 32 characters, first 16 equals to second 16', function() {
-
-      var key = twF.stringToByteArray('supasecret11oneoneeleven')
-        , pt = twF.stringToByteArray('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct)
-        , firstArr = ct.slice(0, 16)
-        , secondArr = ct.slice(16, 32);
-
-      expect(firstArr).not.toEqual(secondArr);
-    });
-
-    it('24 char length key - 32 characters, first 16 equals to second 16 (2)', function() {
-
-      var key = twF.stringToByteArray('supasecret11oneoneeleven')
-        , pt = twF.stringToByteArray('ABCDABCDABCDABCDABCDABCDABCDABCD')
-        , ct = twF.encrypt(key, pt)
-        , cpt = twF.decrypt(key, ct)
-        , firstArr = ct.slice(0, 16)
-        , secondArr = ct.slice(16, 32);
-
-      expect(firstArr).not.toEqual(secondArr);
-    });*/
+        }
+      , checkEquality = function checkEquality(aKey, aPt) {
+
+          var key = twF.stringToByteArray(aKey)
+            , pt = twF.stringToByteArray(aPt)
+            , ct = twF.encryptCBC(key, pt)
+            , firstArr = ct.slice(0, 16)
+            , secondArr = ct.slice(16, 32);
+
+          expect(firstArr).not.toEqual(secondArr);
+        }
+      , tests = [
+          {'testName': '6 char length key - single simple character', 'key': 'secret', 'pt': 'A'},
+          {'testName': '6 char length key - multiple simple characters', 'key': 'secret', 'pt': 'Super secret message!'},
+          {'testName': '6 char length key - single "esotic" character', 'key': 'secret', 'pt': '☭'},
+          {'testName': '6 char length key - multiple "esotic" characters', 'key': 'secret', 'pt': '☢€  ☂ ڴ!'},
+          {'testName': '8 char length key - single simple character', 'key': 'secret12', 'pt': 'A'},
+          {'testName': '8 char length key - multiple simple characters', 'key': 'secret12', 'pt': 'Sunday monday, happy days!'},
+          {'testName': '8 char length key - single "esotic" character', 'key': 'secret12', 'pt': '☂'},
+          {'testName': '8 char length key - multiple "esotic" characters', 'key': 'secret12', 'pt': '☭☢€  ☂ ڴ'},
+          {'testName': '13 char length key - single simple character', 'key': 'anothersecret', 'pt': 'A'},
+          {'testName': '13 char length key - multiple simple characters', 'key': 'anothersecret', 'pt': 'Super secret message!'},
+          {'testName': '13 char length key - single "esotic" character', 'key': 'anothersecret', 'pt': '☭'},
+          {'testName': '13 char length key - multiple "esotic" characters', 'key': 'anothersecret', 'pt': '☢€  ☂ ڴ!'},
+          {'testName': '6 char length key - multiple simple characters', 'key': 'secret', 'pt': 'Super secret message!'},
+          {'testName': '6 char length key - single "esotic" character', 'key': 'secret', 'pt': '☭'},
+          {'testName': '6 char length key - multiple "esotic" characters', 'key': 'secret', 'pt': '☢€  ☂ ڴ!'},
+          {'testName': '8 char length key - single simple character', 'key': 'secret12', 'pt': 'A'},
+          {'testName': '8 char length key - multiple simple characters', 'key': 'secret12', 'pt': 'Sunday monday, happy days!'},
+          {'testName': '8 char length key - single "esotic" character', 'key': 'secret12', 'pt': '☂'},
+          {'testName': '8 char length key - multiple "esotic" characters', 'key': 'secret12', 'pt': '☭☢€  ☂ ڴ'},
+          {'testName': '13 char length key - single simple character', 'key': 'anothersecret', 'pt': 'A'},
+          {'testName': '13 char length key - multiple simple characters', 'key': 'anothersecret', 'pt': 'Super secret message!'},
+          {'testName': '13 char length key - single "esotic" character', 'key': 'anothersecret', 'pt': '☭'},
+          {'testName': '13 char length key - multiple "esotic" characters', 'key': 'anothersecret', 'pt': '☢€  ☂ ڴ!'},
+          {'testName': '37 char length key - single simple character', 'key': 'A Super secret that can not be stolen', 'pt': 'A'},
+          {'testName': '37 char length key - multiple simple characters', 'key': 'A Super secret that can not be stolen', 'pt': 'Sunday monday, happy days!'},
+          {'testName': '37 char length key - single "esotic" character', 'key': 'A Super secret that can not be stolen', 'pt': '☂'},
+          {'testName': '37 char length key - multiple "esotic" characters', 'key': 'A Super secret that can not be stolen', 'pt': '☭☢€  ☂ ڴ'},
+          {'testName': '24 char length key - 32 characters', 'key': 'supasecret11oneoneeleven', 'pt': 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'},
+          {'testName': '24 char length key - 32 characters (2)', 'key': 'supasecret11oneoneeleven', 'pt': 'ABCDABCDABCDABCDABCDABCDABCDABCD'},
+          {'testName': '37 char length key - single simple character', 'key': 'A Super secret that can not be stolen', 'pt': 'A'},
+          {'testName': '37 char length key - multiple simple characters', 'key': 'A Super secret that can not be stolen', 'pt': 'Sunday monday, happy days!'},
+          {'testName': '37 char length key - single "esotic" character', 'key': 'A Super secret that can not be stolen', 'pt': '☂'},
+          {'testName': '37 char length key - multiple "esotic" characters', 'key': 'A Super secret that can not be stolen', 'pt': '☭☢€  ☂ ڴ'},
+          {'testName': '24 char length key - 32 characters', 'key': 'supasecret11oneoneeleven', 'pt': 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'},
+          {'testName': '24 char length key - 32 characters (2)', 'key': 'supasecret11oneoneeleven', 'pt': 'ABCDABCDABCDABCDABCDABCDABCDABCD'}
+        ]
+      , testsLength = tests.length
+      , testIndex = 0
+      , aTest;
+
+    for (; testIndex < testsLength; testIndex += 1) {
+
+      aTest = tests[testIndex];
+      if (aTest) {
+
+        it(aTest.testName, testCase.bind(this, aTest.key, aTest.pt));
+        if (aTest.testName.indexOf('32 characters') >= 0) {
+
+          it('first 16 not equals to second 16', checkEquality.bind(this, aTest.key, aTest.pt));
+        }
+      }
+    }
   });
 }(describe, it, expect, beforeEach, twofish));
